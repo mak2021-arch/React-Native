30eddecf284c4708d442e09e2f4d0a124a0d6b4c
Mohammed Asif <Mohammeda@successive.com>
2021-08-12 18:21:18 +0530

    New Task in food

--- foodmylife/constants/index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable prettier/prettier */
-import icons from './icons';
-//import images from '../images';
-import {COLORS, SIZES} from '../constants/theme';
-
-export {icons, COLORS, SIZES};
